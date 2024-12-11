2e42d55dc44892e333a2d24a9b12895f1c129cea
hong yongjae <developsvai5096@gmail.com>
2024-12-11 20:54:25 +0000

    Refactor : userServic 사용자 회원가입 비지니스로직 작성

--- src/main/java/com/example/userdomain/domain/user/UserService.java
+++ src/main/java/com/example/userdomain/domain/user/UserService.java
@@ -1,4 +1,45 @@
 package com.example.userdomain.domain.user;
 
+import com.example.userdomain.shared.utils.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void createUser(String username, String email, String password, String role, String socialProvider , String phone , String address) {
+
+        if( username != null && email != null && password != null && role != null && socialProvider != null && phone != null ) {
+
+            // 이미 사용자가 존재하는지 확인
+            Optional<UserEntity> existingUser = userRepository.findByEmail(email);
+            if (existingUser.isPresent()) {
+                throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+            }
+
+            // 새 사용자 생성
+            UserEntity user = new UserEntity();
+            user.createUser(username,email,password,role,socialProvider,phone, passwordEncoder);
+            // 사용자 저장
+            userRepository.save(user);
+        } else {
+            throw new IllegalArgumentException("필드가 비어 있습니다.");
+
+        }
+
+    }
+
 }
