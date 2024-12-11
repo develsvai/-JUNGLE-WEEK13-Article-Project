daf9ea84d157758ed06e6684ff3203322165467e
hong yongjae <developsvai5096@gmail.com>
2024-12-11 18:40:31 +0000

    feat : "유저 엔티티 필드 추가 "

--- src/main/java/com/example/userdomain/domain/user/UserEntity.java
+++ src/main/java/com/example/userdomain/domain/user/UserEntity.java
@@ -1,4 +1,24 @@
 package com.example.userdomain.domain.user;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class UserEntity {
+
+    @Id
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+
 }
